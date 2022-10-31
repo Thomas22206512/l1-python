daff06225a6057cc4f7f18ee6cebefc736fdeb00
Thomas Torres <thomas.torres@ens.uvsq.fr>
2022-10-31 23:55:37 +0100

    31/10/22

--- Cestmerde/Object.py
+++ Cestmerde/Object.py
@@ -1,41 +1,4 @@
-class Player:
-    def __init__(self, speudo, health, attack, weapon):
-        self.speudo = speudo
-        self.health = health
-        self.attack = attack
-        self.weapon = None
-        print("Bienvenue au joueur", self.speudo, "/ Points de vie:", self.health, "/ Attaque:", self.attack)
-
-    def get_speudo(self):
-        return self.speudo
-
-    def get_health(self):
-        return self.health
-
-    def get_attack(self):
-        return self.attack 
-
-    def get_weapon(self):
-        return self.weapon
-
-    def damage(self, damage):
-        self.health -= damage
-        print(self.get_speudo(), "Vien de subir", damage, "dégats")
-
-    def attack_player(self, target_player):
-        target_player.damage(self.attack)
-
-class Weapon:
-    def __init__(self, name, damage):
-        self.name = name
-        self.damage = damage
-
-    def get_name(self):
-        return self.name
-
-    def get_damage_amount(self):
-        return self.damage
-
+from model import Player
 knife = Weapon("Knife", 20)
 bombe = Weapon("Bombe", 15)
 
