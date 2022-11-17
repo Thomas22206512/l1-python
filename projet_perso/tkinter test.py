52b43f1c765edf586813cf88945700268c73fdd6
Thomas Torres <thomas.torres@ens.uvsq.fr>
2022-11-17 21:53:05 +0100

    oui

--- projet_perso/tkinter test.py
+++ projet_perso/tkinter test.py
@@ -6,6 +6,6 @@ window.title("Générateur de Bitcoin Facile et GRATUIT 100% no fake")
 window.geometry("400x500")
 window.minsize(300, 300)
 window.maxsize(1080, 720)
-window.iconbitmap("C:\Users\thoma\l1-phyton-1\projet perso\logoico.ico")
+window.iconbitmap('logo.ico')
 #afficher
 window.mainloop()
