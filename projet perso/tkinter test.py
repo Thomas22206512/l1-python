9ddbce935fba77648de50de8c7dfb8b738b9a112
Thomas Torres <thomas.torres@ens.uvsq.fr>
2022-11-17 21:49:53 +0100

    o

--- projet perso/tkinter test.py
+++ projet perso/tkinter test.py
@@ -1,5 +1,10 @@
 from tkinter import *
-import random
-root = Tk()
-
-root.mainloop()
+#Créer une première fenêtre
+window = Tk()
+#personnaliser cette fenêtre
+window.title("Générateur de Bitcoin Facile et GRATUIT 100% no fake")
+window.geometry("400x500")
+window.minsize(300, 300)
+window.maxsize(1080, 720)
+#afficher
+window.mainloop()
